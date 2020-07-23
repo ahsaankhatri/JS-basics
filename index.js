e64456e63c075b9367652f0025bc6e1b7db0db9c
ahsaankhatri <62430260+ahsaankhatri@users.noreply.github.com>
2020-07-23 18:29:11 +0500

    Added new files

---
+++ index.js
@@ -0,0 +1,31 @@
+// console.log thing!
+console.log('Hi Team, My fisrt JS code!');
+
+//defining and declaring Varaibles
+let name = '\nAhsaan';
+let age = 20;
+console.log(name);
+console.log(age);
+
+//contents
+const Pi = 3.142;
+console.log('\n',Pi);
+
+//Object
+let teacher = {
+    name: 'Ahmed',
+    age: '28',
+    qualification: 'Master'
+}
+console.log('\n', teacher);
+
+//Arrays
+let progLanguages = ['C','C++','Python','JavaScript','C#','Java'];
+console.log('\n',progLanguages);
+console.log('\n',progLanguages[3]);
+
+//function
+function display(firstName, lastName) {
+    console.log('\nHi '+ firstName + ' ' + lastName);
+}
+display('Ahsaan', 'Khatri');
